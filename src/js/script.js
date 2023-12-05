81ac67783345e91fb957fd2e67ca4c263b3fbe3a
daniil <daniilkurgguzenko132@gmail.com>
2023-12-05 19:01:52 +0300

    no disign

--- src/js/script.js
+++ src/js/script.js
@@ -20,6 +20,40 @@ function expandFormMini() {
 
 }
 
+function showInput() {
+    var select = document.getElementById("transport");
+    var carInput = document.getElementById("carInput");
+    var busInput = document.getElementById("busInput");
+    var tractorInput = document.getElementById("tractorInput");
+    var truckInput = document.getElementById("truckInput");
+    var motorcycleInput = document.getElementById("motorcycleInput");
 
+    // Скрыть все input поля
+    carInput.style.display = "none";
+    busInput.style.display = "none";
+    tractorInput.style.display = "none";
+    truckInput.style.display = "none";
+    motorcycleInput.style.display = "none";
 
-
+    // Показать соответствующее input поле в зависимости от выбранной опции
+    switch (select.value) {
+      case "car":
+        carInput.style.display = "block";
+        break;
+      case "bus":
+        busInput.style.display = "block";
+        break;
+      case "tractor":
+        tractorInput.style.display = "block";
+        break;
+      case "truck":
+        truckInput.style.display = "block";
+        break;
+      case "motorcycle":
+        motorcycleInput.style.display = "block";
+        break;
+      default:
+        // Действия по умолчанию или для других значений
+        break;
+    }
+  }
